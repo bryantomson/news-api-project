ba28ebe0ae65a4b5796c63a375d4be8ebbf45cf7
Tom Bryson <tom@Toms-Mac-mini-2.lan>
2023-11-24 15:47:17 +0000

    add /api/users/:username endpoint

--- models/users-model.js
+++ models/users-model.js
@@ -5,3 +5,14 @@ exports.selectUsers = () => {
     return rows;
   });
 };
+exports.selectUserByUsername = (username) => {
+  console.log("model", username);
+  return db
+    .query(`SELECT * FROM users WHERE name = $1;`, [username])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "not found" });
+      }
+      return rows[0];
+    });
+};
