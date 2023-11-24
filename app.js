59232a8f31a9d48d815728c4dbdbbe7077fa6276
Tom Bryson <tom@Toms-Mac-mini-2.lan>
2023-11-24 15:16:54 +0000

    add express routers to break down endpoints

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const {
-  getTopics,
-  getUsers,
-  getEndpoints,
-  getArticles,
-  getArticleById,
-  getCommentsByArticleId,
-  postCommentByArticleId,
-  patchArticleById,
-  deleteCommentById,
-} = require("./controller");
+
 const { handleCustomErrors, handle500, handleNotFound } = require("./errors");
+const apiRouter = require('./routes/api-router');
 
 const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/api/topics", getTopics);
+app.use("/api", apiRouter);
 
-app.get("/api/users", getUsers);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api", getEndpoints);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.post("/api/articles/:article_id/comments", postCommentByArticleId);
-
-app.patch("/api/articles/:article_id", patchArticleById);
-
-app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.get("*", (req, res, next) => {
   next({ status: 404, msg: "path not found" });
---
+++ routes/topics-router.js
@@ -0,0 +1,7 @@
+const topicsRouter = require("express").Router();
+
+const { getTopics } = require("../controller");
+
+topicsRouter.get("/", getTopics);
+
+module.exports = topicsRouter;
